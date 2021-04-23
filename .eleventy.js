ad7ac5a8dc632e4b9d9a61a5f244ebb34bcd8ff9
Bram Smulders <source@bram.is>
2021-04-23 13:44:35 +0200

    eleventy source folder change

--- .eleventy.js
+++ .eleventy.js
@@ -1,21 +1,21 @@
 // Import plugins
-const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
-const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const eleventyNavigationPlugin = require('@11ty/eleventy-navigation');
+const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
 
 // Import transforms
-const htmlMinTransform = require("./_docs/src/transforms/html-min-transform.js");
-const parseTransform = require("./_docs/src/transforms/parse-transform.js");
+const htmlMinTransform = require('./_docs/src/transforms/html-min-transform.js');
+const parseTransform = require('./_docs/src/transforms/parse-transform.js');
 
 // Import shortcodes
-const demoShortcode = require("./_docs/src/shortcodes/demo.js");
+const demoShortcode = require('./_docs/src/shortcodes/demo.js');
 
 module.exports = function (config) {
   // shortcodes
-  config.addPairedShortcode("demo", demoShortcode);
+  config.addPairedShortcode('demo', demoShortcode);
 
   // Transforms
-  config.addTransform("htmlmin", htmlMinTransform);
-  config.addTransform("parse", parseTransform);
+  config.addTransform('htmlmin', htmlMinTransform);
+  config.addTransform('parse', parseTransform);
 
   // Add plugins
   config.addPlugin(eleventyNavigationPlugin);
@@ -24,8 +24,10 @@ module.exports = function (config) {
   // Base eleventyConfig
   return {
     dir: {
-      input: "_docs/src/templates",
-      output: "_docs/dist",
+      input: './',
+      includes: '_docs/src/templates/_includes',
+      data: '_docs/src/templates/_data',
+      output: '_docs/dist',
     },
     passthroughFileCopy: true,
   };
